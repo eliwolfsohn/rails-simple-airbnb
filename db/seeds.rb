db01459350553fcabb92d78a784f571b3fc533d9
Eli Wolfsohn <eliwolfsohn@gmail.com>
2019-09-03 18:53:02 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Paris Home',
+  address: 'Rawer',
+  description: 'Nope',
+  price_per_night: 100,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Longon',
+  address: 'RLdh',
+  description: 'hyee',
+  price_per_night: 10,
+  number_of_guests: 10
+)
+
+Flat.create!(
+  name: 'HIjhfnd',
+  address: 'Bkahh',
+  description: 'ghaudfjno',
+  price_per_night: 3,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Happp',
+  address: 'YEAH',
+  description: 'Boom',
+  price_per_night: 8,
+  number_of_guests: 1
+)
